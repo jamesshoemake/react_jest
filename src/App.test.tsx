7ac0736499854ac52c020cc286fa9e12dd292e45
James Shoemake <james.shoemake1@gmail.com>
2022-08-17 09:42:34 -0500

    added notes to basic app test example

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
+// test(name, fn, timeout)
+// name: name of test
+// fn -> function contains expectation of the test
+// timeout is optional default 5seconds
+// test and expect are globally provided by create react app
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />) // create virtual dom with element needed for test
+  const linkElement = screen.getByText(/learn react/i) // element to assert against
+  expect(linkElement).toBeInTheDocument() // matcher function to perform assertion
+})
