650cb676a3833428251b6cc874cf8b9dcefe623f
James Shoemake <james.shoemake1@gmail.com>
2022-08-24 09:09:33 -0500

    remove __tests__ folder and greet.spec.tsx alternative testing. Added coverage script and adjusted code to fail 80% global threshold.

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,5 +1,7 @@
 // Greet should render text hello
 // should render hello (name) if passed a name
+// file could also be named greet.spec.tsx
+// also might use __tests__ folder to store tests
 
 import { render, screen } from '@testing-library/react'
 import { Greet } from './greet'
@@ -11,22 +13,22 @@ import { Greet } from './greet'
 describe('Greet', () => {
   test('render correctly', () => {
     render(<Greet />)
-    const textEl = screen.getByText('Hello')
+    const textEl = screen.getByText(/Hello/)
     expect(textEl).toBeInTheDocument()
   })
-  describe('Nested', () => {
-    test('renders with name', () => {
-      render(<Greet name="John" />)
-      const textEl = screen.getByText('Hello John')
-      expect(textEl).toBeInTheDocument()
-    })
-  })
+  // describe('Nested', () => {
+  //   test('renders with name', () => {
+  //     render(<Greet name="John" />)
+  //     const textEl = screen.getByText('Hello John')
+  //     expect(textEl).toBeInTheDocument()
+  //   })
+  // })
 })
 
-describe('UnNested-same file', () => {
-  test('renders with name-2', () => {
-    render(<Greet name="John" />)
-    const textEl = screen.getByText('Hello John')
-    expect(textEl).toBeInTheDocument()
-  })
-})
+// describe('UnNested-same file', () => {
+//   test('renders with name-2', () => {
+//     render(<Greet name="John" />)
+//     const textEl = screen.getByText('Hello John')
+//     expect(textEl).toBeInTheDocument()
+//   })
+// })
