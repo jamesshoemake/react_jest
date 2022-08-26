c969e017a302caf08111131f39d668e3bfc8e30c
James Shoemake <james.shoemake1@gmail.com>
2022-08-26 09:22:32 -0500

    added notes on asertions and matchers

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -14,21 +14,26 @@ describe('Greet', () => {
   test('render correctly', () => {
     render(<Greet />)
     const textEl = screen.getByText(/Hello/)
+    // asertion uses expect keyword
+    // expect is called with a matcher function such as toBeInTheDocument
+    // jestjs.io/docs/using-matchers contains common matchers
+    // github.com/testing-library/jest-dom for other custom matchers
+    // jest-dom is installed when using npx create-react-app
     expect(textEl).toBeInTheDocument()
   })
-  // describe('Nested', () => {
-  //   test('renders with name', () => {
-  //     render(<Greet name="John" />)
-  //     const textEl = screen.getByText('Hello John')
-  //     expect(textEl).toBeInTheDocument()
-  //   })
-  // })
+  describe('Nested', () => {
+    test('renders with name', () => {
+      render(<Greet name="John" />)
+      const textEl = screen.getByText('Hello John')
+      expect(textEl).toBeInTheDocument()
+    })
+  })
 })
 
-// describe('UnNested-same file', () => {
-//   test('renders with name-2', () => {
-//     render(<Greet name="John" />)
-//     const textEl = screen.getByText('Hello John')
-//     expect(textEl).toBeInTheDocument()
-//   })
-// })
+describe('UnNested-same file', () => {
+  test('renders with name-2', () => {
+    render(<Greet name="John" />)
+    const textEl = screen.getByText('Hello John')
+    expect(textEl).toBeInTheDocument()
+  })
+})
