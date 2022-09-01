ad6c63c12fc829ec96dda7f5009367cbc27f42e2
James Shoemake <15272934+jamesshoemake@users.noreply.github.com>
2022-09-01 10:26:50 -0500

    added examples for getByTitle, getByTestId, and additional notes for usage.

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -1,56 +1,78 @@
-import { Application } from './application'
-import { render, screen } from '@testing-library/react'
-import { isExportDeclaration } from 'typescript'
+import { Application } from "./application"
+import { render, screen } from "@testing-library/react"
 
-describe('Application', () => {
-  test('renders correctly', () => {
+// When to use a query
+// resemble how users interact with the code
+
+// - most common queries
+// 1. getByRole - top preference for almost everything
+// 2. getByLabelText - second best, forms work great with this one.
+// 3. getByPlaceholderText - if no label, use placeholder for element that contain them
+// 4. getByText - can find non interactive elements
+// 5. getByDisplayValue - nav for page with filled values
+
+// - if needed - UX for the below varies by browsers - screen reader etc
+// 6. getByAltText
+// 7. getByTitle
+
+// - last chance
+// 8. getByTestId - user cannot see or hear these, dynamic text areas
+
+describe("Application", () => {
+  test("renders correctly", () => {
     render(<Application />)
 
-    const pageHeading = screen.getByRole('heading', {
-      level: 1
+    const pageHeading = screen.getByRole("heading", {
+      level: 1,
     })
     expect(pageHeading).toBeInTheDocument()
 
-    const sectionHeading = screen.getByRole('heading', {
-      level: 2
+    const sectionHeading = screen.getByRole("heading", {
+      level: 2,
     })
     expect(sectionHeading).toBeInTheDocument()
 
-    const nameEl = screen.getByRole('textbox', {
-      name: 'Name'
+    const closeEl = screen.getByTitle("close")
+    expect(closeEl).toBeInTheDocument()
+
+    const nameEl = screen.getByRole("textbox", {
+      name: "Name",
     })
     expect(nameEl).toBeInTheDocument()
 
-    const nameEl2 = screen.getByLabelText('Name')
+    const nameEl2 = screen.getByLabelText("Name")
     expect(nameEl2).toBeInTheDocument()
 
-    const nameEl3 = screen.getByPlaceholderText('Fullname')
+    const nameEl3 = screen.getByPlaceholderText("Fullname")
     expect(nameEl3).toBeInTheDocument()
 
-    const termEl2 = screen.getByLabelText('I agree to the terms and conditions')
+    const termEl2 = screen.getByLabelText("I agree to the terms and conditions")
     expect(termEl2).toBeInTheDocument()
 
-    const nameEl4 = screen.getByDisplayValue('leChonk')
+    const nameEl4 = screen.getByDisplayValue("leChonk")
     expect(nameEl4).toBeInTheDocument()
 
-    const imgEl = screen.getByAltText('a person with a laptop')
+    const imgEl = screen.getByAltText("a person with a laptop")
     expect(imgEl).toBeInTheDocument()
 
-    const bioEl = screen.getByRole('textbox', {
-      name: 'Bio'
+    const bioEl = screen.getByRole("textbox", {
+      name: "Bio",
     })
     expect(bioEl).toBeInTheDocument()
 
-    const paraEl = screen.getByText('All fields are mandatory')
+    const paraEl = screen.getByText("All fields are mandatory")
     expect(paraEl).toBeInTheDocument()
 
-    const jobLocationEl = screen.getByRole('combobox')
+    const dataId = screen.getByTestId("custom-element")
+    expect(dataId).toBeInTheDocument()
+
+    const jobLocationEl = screen.getByRole("combobox")
     expect(jobLocationEl).toBeInTheDocument()
 
-    const termsEl = screen.getByRole('checkbox')
+    const termsEl = screen.getByRole("checkbox")
     expect(termsEl).toBeInTheDocument()
 
-    const submitBtn = screen.getByRole('button')
+    const submitBtn = screen.getByRole("button")
     expect(submitBtn).toBeInTheDocument()
   })
 })
