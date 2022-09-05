2ded7bd857a01b4eb8f2b464151f4a59d2d92d42
James Shoemake <15272934+jamesshoemake@users.noreply.github.com>
2022-09-05 09:19:10 -0500

    Added new test that uses a queryBy to check if an element exist.

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -15,4 +15,23 @@ describe("Skills", () => {
     const listItemEls = screen.getAllByRole("listitem")
     expect(listItemEls).toHaveLength(skills.length)
   })
+
+  test("renders login button", () => {
+    render(<Skills skills={skills} />)
+    const loginBtn = screen.getByRole("button", {
+      name: "Login",
+    })
+    expect(loginBtn).toBeInTheDocument()
+  })
+
+  // queryBy and queryAllBy
+  // queryBy return null if does not exist
+  // queryAllBy return empty array if does not exist
+  test("start learning button is not", () => {
+    render(<Skills skills={skills} />)
+    const startLearningBtn = screen.queryByRole("button", {
+      name: "Start Learning",
+    })
+    expect(startLearningBtn).not.toBeInTheDocument()
+  })
 })
