5a7dd15ddfb101d3755ed0ba8067de06b6ae35b2
James Shoemake <james.shoemake1@gmail.com>
2022-09-06 09:37:04 -0500

    Added final class of query methods: findBy.

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -1,25 +1,25 @@
-import { render, screen } from "@testing-library/react"
-import { Skills } from "./skills"
+import { render, screen } from '@testing-library/react'
+import { Skills } from './skills'
 
-describe("Skills", () => {
-  const skills = ["HTML", "CSS", "JavaScript"]
+describe('Skills', () => {
+  const skills = ['HTML', 'CSS', 'JavaScript']
 
-  test("Renders correctly", () => {
+  test('Renders correctly', () => {
     render(<Skills skills={skills} />)
-    const listEl = screen.getByRole("list")
+    const listEl = screen.getByRole('list')
     expect(listEl).toBeInTheDocument()
   })
 
-  test("renders a list of skills", () => {
+  test('renders a list of skills', () => {
     render(<Skills skills={skills} />)
-    const listItemEls = screen.getAllByRole("listitem")
+    const listItemEls = screen.getAllByRole('listitem')
     expect(listItemEls).toHaveLength(skills.length)
   })
 
-  test("renders login button", () => {
+  test('renders login button', () => {
     render(<Skills skills={skills} />)
-    const loginBtn = screen.getByRole("button", {
-      name: "Login",
+    const loginBtn = screen.getByRole('button', {
+      name: 'Login'
     })
     expect(loginBtn).toBeInTheDocument()
   })
@@ -27,11 +27,28 @@ describe("Skills", () => {
   // queryBy and queryAllBy
   // queryBy return null if does not exist
   // queryAllBy return empty array if does not exist
-  test("start learning button is not", () => {
+  test('start learning button is not', () => {
     render(<Skills skills={skills} />)
-    const startLearningBtn = screen.queryByRole("button", {
-      name: "Start Learning",
+    const startLearningBtn = screen.queryByRole('button', {
+      name: 'Start Learning'
     })
     expect(startLearningBtn).not.toBeInTheDocument()
+  })
+
+  // findBy|findAllBy returns a promise
+  // default timeout of 1000 milliseconds
+  // 3rd arg object with timeout overrides the default timeout
+  test('Start learning button is eventually displayed', async () => {
+    render(<Skills skills={skills} />)
+    const startLearningBtn = await screen.findByRole(
+      'button',
+      {
+        name: 'Start Learning'
+      },
+      {
+        timeout: 2000
+      }
+    )
+    expect(startLearningBtn).toBeInTheDocument()
   })
 })
