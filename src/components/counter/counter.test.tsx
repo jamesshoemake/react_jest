76974e8ff548aae520b22bbe63c8ec00dd6e3b84
James Shoemake <james.shoemake1@gmail.com>
2022-09-12 10:07:31 -0500

    add basic keyboard interactions

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -39,4 +39,35 @@ describe('Counter', () => {
     const counterEl = screen.getByRole('heading')
     expect(counterEl).toHaveTextContent('2')
   })
+
+  test('renders a count of 10 after clicking set btn', async () => {
+    render(<Counter />)
+    const amountInput = screen.getByRole('spinbutton')
+    await user.type(amountInput, '10')
+    expect(amountInput).toHaveValue(10)
+
+    const setBtn = screen.getByRole('button', {
+      name: 'Set'
+    })
+    await user.click(setBtn)
+    const counterEl = screen.getByRole('heading')
+    expect(counterEl).toHaveTextContent('10')
+  })
+
+  test('elements are focused in correct order', async () => {
+    render(<Counter />)
+    const amountInput = screen.getByRole('spinbutton')
+    const setBtn = screen.getByRole('button', {
+      name: 'Set'
+    })
+    const incrementBtn = screen.getByRole('button', {
+      name: 'Increment'
+    })
+    await user.tab()
+    expect(incrementBtn).toHaveFocus()
+    await user.tab()
+    expect(amountInput).toHaveFocus()
+    await user.tab()
+    expect(setBtn).toHaveFocus()
+  })
 })
